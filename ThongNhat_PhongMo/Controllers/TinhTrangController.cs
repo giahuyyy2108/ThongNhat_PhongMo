e08e292e69e536c5d1517b502b602282cb401e8e
Gia Huy <vuhuy110@outlook.com>
2024-02-21 07:57:01 +0700

    Final

---
+++ ThongNhat_PhongMo/Controllers/TinhTrangController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ThongNhat_PhongMo.Models;
+
+namespace ThongNhat_PhongMo.Controllers
+{
+    public class TinhTrangController : Controller
+    {
+        private readonly DataBaseContext _context;
+
+        public TinhTrangController(DataBaseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TinhTrang
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.tinhtrang
+                .Where(p=>p.id < 5)                
+                .ToListAsync());
+        }
+
+        // GET: TinhTrang/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tinhTrang = await _context.tinhtrang
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (tinhTrang == null)
+            {
+                return NotFound();
+            }
+
+            return View(tinhTrang);
+        }
+
+        // GET: TinhTrang/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TinhTrang/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,Name")] TinhTrang tinhTrang)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tinhTrang);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tinhTrang);
+        }
+
+        // GET: TinhTrang/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tinhTrang = await _context.tinhtrang.FindAsync(id);
+            if (tinhTrang == null)
+            {
+                return NotFound();
+            }
+            return View(tinhTrang);
+        }
+
+        // POST: TinhTrang/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,Name")] TinhTrang tinhTrang)
+        {
+            if (id != tinhTrang.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tinhTrang);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TinhTrangExists(tinhTrang.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tinhTrang);
+        }
+
+        // GET: TinhTrang/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tinhTrang = await _context.tinhtrang
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (tinhTrang == null)
+            {
+                return NotFound();
+            }
+
+            return View(tinhTrang);
+        }
+
+        // POST: TinhTrang/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var tinhTrang = await _context.tinhtrang.FindAsync(id);
+            _context.tinhtrang.Remove(tinhTrang);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TinhTrangExists(int id)
+        {
+            return _context.tinhtrang.Any(e => e.id == id);
+        }
+    }
+}
